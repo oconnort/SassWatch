f52f5f57a9087fbdc07f2547c675a027e86c36e2
Tom O'Connor <thomas.andrew.oconnor@gmail.com>
2012-10-17 20:57:53 +0100

    Initial cut of codebase

---
+++ README.md
@@ -0,0 +1,25 @@
+# SASS Watch
+
+A [Sublime Text 2](http://www.sublimetext.com/) plugin for watching Sass files.
+
+## Manual installation
+
+Go to the "Packages" directory (`Preferences` / `Browse Packages…`). Then clone this repository:
+
+    git clone git://github.com/oconnort/SassWatch
+
+# Usage
+
+All functionality of the plugin is available in the `Tools` / `SassWatch` menu and in the command pallette.
+
+# Default key bindings:
+
+## Watch Current Sass File
+
+* Windows and Linux: `Ctrl+ALT+W`
+
+# Information
+
+Source: https://github.com/condemil/Gist
+
+Author: [Tom O'Connor](https://github.com/oconnort/)
---
+++ SassWatch.py
@@ -0,0 +1,35 @@
+import os
+import sublime
+import sublime_plugin
+import subprocess
+
+# Plugin to launch a SASS command to watch the current file and
+# let the user input the output file name & location
+class WatchCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+
+        def fileNameInput(output_css_file):
+
+            # Check that output CSS file has correct extension
+            if output_css_file.rfind(".css") == -1:
+                sublime.status_message("Please enter a CSS file name.")
+                return
+
+            activeFile = sublime.active_window().active_view().file_name()
+
+            # Check if file is a SASS file
+            if activeFile.rfind(".scss") == -1:
+                sublime.status_message("Plugin only works on files with .scss/.sass extensions.")
+                return
+
+            index = activeFile.rfind("\\")
+            sass_file = activeFile[(index+1): ]
+            sass_file_dir = activeFile[ : index]
+            plugin_dir = os.path.join(sublime.packages_path(), 'User')
+
+            # Run our batch file
+            subprocess.Popen(['cmd', '/c', 'start', 'sasswatch.bat', sass_file_dir, sass_file, output_css_file], cwd=plugin_dir, shell=True)
+            #process.ProcessOpen(['cmd', '/c', 'start', 'sasswatch.bat', sass_file_dir, sass_file, output_css_file], cwd=plugin_dir)
+
+        self.view.window().show_input_panel('CSS Output File (Relative or Absolute Path):', '', fileNameInput, None, None)
